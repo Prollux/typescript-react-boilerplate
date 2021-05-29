b16245380f7ffb75967479d12d8d99a509b4d524
Zachary Shold <sholdzb@gmail.com>
2021-05-29 18:31:42 -0500

    removes console.log from server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import * as path from 'path'
 import * as express from 'express'
 const port = 3080
 const app = express()
-console.log(__dirname)
+
 app.use('/', express.static(path.join(__dirname, '../build')))
 
 app.listen(port, () => {
